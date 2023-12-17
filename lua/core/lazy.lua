2ee4308ff8c78ee5fd9c6e08dc9c8dc6ac2c0349
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-17 12:06:11 +0700

    update

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
 	},
 	defaults = {
 		lazy = true, -- every plugin is lazy-loaded by default
-		-- version = "*", -- try installing the latest stable version for plugins that support semver
 	},
 	ui = { border = "rounded", browser = "chrome", throttle = 40, custom_keys = { ["<localleader>l"] = false } },
 	change_detection = { enabled = true, notify = false },
