6c29f1c698a7f1a85ee002ead528c62c6e361c0f
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-23 19:38:44 +0700

    update

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -59,8 +59,8 @@ return {
                 val = {
                     button("SPC f f", "  Find File  ", ":Telescope find_files<CR>"),
                     button("SPC f o", "  Recent File  ", ":Telescope oldfiles<CR>"),
-                    button("SPC f w", "  Find Word  ", ":Telescope live_grep<CR>"),
-                    button("SPC b m", "  Bookmarks  ", ":Telescope marks<CR>"),
+                    button("SPC f w", "  Find Word  ", ":Telescope live_grep theme=ivy<CR>"),
+                    button("SPC b m", "  Bookmarks  ", ":Telescope marks theme=ivy<CR>"),
                     button("SPC t h", "  Themes  ", ":Telescope colorscheme theme=dropdown<CR>"),
                     button("SPC e s", "  Settings", ":e $MYVIMRC | :cd %:p:h <CR>"),
                 },
