b165dbe0017a3620f4271622dcbf936c4c2701c0
nhattruongNeoVim <nhattruong13112000@gmail.com>
2023-12-30 19:22:37 +0700

    update

--- lua/plugins/more.lua
+++ lua/plugins/more.lua
@@ -42,9 +42,6 @@ return {
 		keys = {
 			{ "H", "<cmd>TSJToggle<cr>", desc = "Join Toggle" },
 		},
-		opts = {
-			use_default_keymaps = false,
-			max_join_length = 150,
-		},
+		opts = { use_default_keymaps = false, max_join_length = 150 },
 	},
 }
