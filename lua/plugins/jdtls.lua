20bfc8fa4ad33a9b9a405b58f88cd1d0489146b0
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-01-19 08:40:47 +0700

    update

--- lua/plugins/jdtls.lua
+++ lua/plugins/jdtls.lua
@@ -137,25 +137,3 @@ return {
 		)
 	end,
 }
--- return {
---     "nvim-java/nvim-java",
---     event = "VimEnter",
---     dependencies = {
---         "nvim-java/lua-async-await",
---         "nvim-java/nvim-java-core",
---         "nvim-java/nvim-java-test",
---         "nvim-java/nvim-java-dap",
---         "MunifTanjim/nui.nvim",
---         "neovim/nvim-lspconfig",
---         "mfussenegger/nvim-dap",
---         {
---             "williamboman/mason.nvim",
---             opts = {
---                 registries = {
---                     "github:nvim-java/mason-registry",
---                     "github:mason-org/mason-registry",
---                 },
---             },
---         },
---     },
--- }
