53c9dfd3b9f77fe8fb50c4e478de048695884d50
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-01-27 20:58:48 +0700

    update

--- lua/core/option2.lua
+++ lua/core/option2.lua
@@ -8,23 +8,19 @@
 -- https://neovim.io/doc/user/vim_diff.html#nvim-defaults
 
 local my_opt = {
-    -- Location in the buffer
-    number         = true,
-    relativenumber = true,
-    cursorline     = false,
-    cursorcolumn   = false,
-    cursorlineopt  = "number",
-
-    -- Update time
-    updatetime     = 250, --> specifies the update time interval in milliseconds for recording changes
-    timeoutlen     = 300, --> this determines how frequently NeoVim saves changes to the undo buffer.
-
     -- Tab
     tabstop        = 4,    --> how many characters Vim /treats/renders/ <TAB> as
     shiftwidth     = 4,    --> number of spaces to use for auto-indentation, <<, >>, etc
     softtabstop    = 0,    --> how many chracters the /cursor moves/ with <TAB> and <BS> -- 0 to disable
     expandtab      = true, --> use space instead of tab
     autoindent     = true, --> enable automatic indentation for new lines based on the previous line
+
+    -- Location in the buffer
+    number         = true,
+    relativenumber = true,
+    cursorline     = false,
+    cursorcolumn   = false,
+    cursorlineopt  = "number",
 
     -- Search
     ignorecase     = true, --> ignore case in search
@@ -33,12 +29,6 @@ local my_opt = {
     -- Split
     splitright     = true, --> vertical split created right
     splitbelow     = true, --> horizontal split created below
-
-    -- Spell
-    spell          = false,    --> autocmd will enable spellcheck in Tex or markdown
-    spelllang      = "en",     --> lang
-    spellsuggest   = "best,8", --> suggest 8 words for spell suggestion
-    spelloptions   = "camel",  --> consider CamelCase when checking spelling
 
     -- UI
     signcolumn     = "yes",             --> render signcolumn
@@ -49,26 +39,38 @@ local my_opt = {
     guifont        = "MesloLGS NF:h10", --> font in neovim-qt
     cmdheight      = 1,                 --> size of neovim command line for display command
 
+    -- Spell
+    spell          = false,    --> autocmd will enable spellcheck in Tex or markdown
+    spelllang      = "en",     --> lang
+    spellsuggest   = "best,8", --> suggest 8 words for spell suggestion
+    spelloptions   = "camel",  --> consider CamelCase when checking spelling
+
     -- Fold
     foldenable     = false,                        --> auto folds until close them using zc/zC or update using zx
     foldmethod     = "expr",                       --> use `foldexpr` function for folding
     foldexpr       = "nvim_treesitter#foldexpr()", --> treesitter folding
     foldlevel      = 1,                            --> ignore n - 1 level fold
 
+    -- Update time
+    updatetime     = 250, --> specifies the update time interval in milliseconds for recording changes
+    timeoutlen     = 300, --> this determines how frequently NeoVim saves changes to the undo buffer.
+
     -- Others
-    mouse          = "a",                              --> allow mouse in all mode
-    confirm        = true,                             --> confirm before exiting with unsaved bufffer(s)
-    fillchars      = { eob = " " },                    --> delete ~ on empty line
-    wrap           = false,                            --> wrap code
-    mousemoveevent = true,                             --> enable mouse move events to track mouse movements
-    iskeyword      = vim.opt.iskeyword + "-",          --> append '-' to the list of characters considered as keyword parts
-    clipboard      = vim.opt.clipboard + "unnamedplus" --> add clipboard
+    mouse          = "a",           --> allow mouse in all mode
+    confirm        = true,          --> confirm before exiting with unsaved bufffer(s)
+    fillchars      = { eob = " " }, --> delete ~ on empty line
+    wrap           = false,         --> wrap code
+    mousemoveevent = true,          --> enable mouse move events to track mouse movements
+    -- "iskeyword:append("-")",
 }
 
 local opt = vim.opt
 for key, val in pairs(my_opt) do
     opt[key] = val
 end
+
+vim.opt.clipboard:append("unnamedplus") --> clipboard
+vim.opt.iskeyword:append("-")           --> append '-' to the list of characters considered as keyword parts
 
 --------------------------------------------------------- CMD  ---------------------------------------------------------
 -- autoreload file when it changes outside of NeoVim
