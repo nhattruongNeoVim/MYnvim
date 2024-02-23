572e820685dbe45999f7fe2c08a639e606d8c1b9
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-02-23 19:12:47 +0700

    update

--- lua/plugins/lsp/lsp.lua
+++ lua/plugins/lsp/lsp.lua
@@ -61,13 +61,6 @@ return {
 			},
 			symbol_in_winbar = {
 				enable = false,
-				separator = " ",
-				ignore_patterns = {},
-				hide_keyword = true,
-				show_file = true,
-				folder_level = 2,
-				respect_root = false,
-				color_mode = true,
 			},
 		})
 
