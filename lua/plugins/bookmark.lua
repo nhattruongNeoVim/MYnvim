0d1934199ddcf19ce5fefcbd768f72647ce17790
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-02-23 19:57:51 +0700

    update

--- lua/plugins/bookmark.lua
+++ lua/plugins/bookmark.lua
@@ -7,7 +7,7 @@ return {
 			keywords = {
 				["@t"] = " ", -- mark annotation startswith @t ,signs this icon as `Todo`
 				["@w"] = " ", -- mark annotation startswith @w ,signs this icon as `Warn`
-				["@f"] = "⛏ ", -- mark annotation startswith @f ,signs this icon as `Fix`
+				["@f"] = "󰣪 ", -- mark annotation startswith @f ,signs this icon as `Fix`
 				["@n"] = " ", -- mark annotation startswith @n ,signs this icon as `Note`
 			},
 			signs = {
