76e2c28226f611f28eade99a69b1e7ad98af8088
nhattruongNeoVim <nhattruong13112000@gmail.com>
2024-03-05 19:32:36 +0700

    update

---
+++ lua/plugins/codesnap.lua
@@ -0,0 +1,15 @@
+return {
+	"mistricky/codesnap.nvim",
+	build = "make",
+	cmd = "CodeSnapPreviewOn",
+	config = function()
+		require("codesnap").setup({
+			mac_window_bar = true, -- (Optional) MacOS style title bar switch
+			opacity = true, -- (Optional) The code snap has some opacity by default, set it to false for 100% opacity
+			watermark = "nhattruongNeoVim", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
+			preview_title = "CodeSnap.nvim", -- (Optional) preview page title
+			editor_font_family = "MesloLGS NF", -- (Optional) preview code font family
+			watermark_font_family = "Pacifico", -- (Optional) watermark font family
+		})
+	end,
+}
