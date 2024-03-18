7b70383c70f3b35cf43ed84bb2ef8da338813431
Lê Nhật Trường <126251176+nhattruongNeoVim@users.noreply.github.com>
2024-03-18 15:32:13 +0700

    Update codesnap.lua

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -1,16 +1,16 @@
-return {
-    -- NOTE: press 'gb' on 	"mistricky/codesnap.nvim" on lazy menu when build fails.
-	"mistricky/codesnap.nvim",
-	build = "make",
-	cmd = "CodeSnapPreviewOn",
-	config = function()
-		require("codesnap").setup({
-			mac_window_bar = true, -- (Optional) MacOS style title bar switch
-			opacity = true, -- (Optional) The code snap has some opacity by default, set it to false for 100% opacity
-			watermark = "nhattruongNeoVim", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
-			preview_title = "CodeSnap.nvim", -- (Optional) preview page title
-			editor_font_family = "MesloLGS NF", -- (Optional) preview code font family
-			watermark_font_family = "Pacifico", -- (Optional) watermark font family
-		})
-	end,
-}
+--return {
+--    -- NOTE: press 'gb' on 	"mistricky/codesnap.nvim" on lazy menu when build fails.
+--	"mistricky/codesnap.nvim",
+--	build = "make",
+--	cmd = "CodeSnapPreviewOn",
+--	config = function()
+--		require("codesnap").setup({
+--			mac_window_bar = true, -- (Optional) MacOS style title bar switch
+--			opacity = true, -- (Optional) The code snap has some opacity by default, set it to false for 100% opacity
+--			watermark = "nhattruongNeoVim", -- (Optional) you can custom your own watermark, but if you don't like it, just set it to ""
+--			preview_title = "CodeSnap.nvim", -- (Optional) preview page title
+--			editor_font_family = "MesloLGS NF", -- (Optional) preview code font family
+--			watermark_font_family = "Pacifico", -- (Optional) watermark font family
+--		})
+--	end,
+--}
